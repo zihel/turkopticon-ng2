4985567d2e26830977b608ff1990db9b8f5e7c3c
Yuankai Li <ykzihel@gmail.com>
2016-06-20 14:01:11 -0400

    refactor: remove form testing from main component and add it into review overview

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,16 @@
 import { Component } from '@angular/core';
 import { ROUTER_DIRECTIVES } from '@angular/router';
 
-import { ReviewFormComponent } from './review-form';
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css'],
-  directives: [ROUTER_DIRECTIVES, ReviewFormComponent]
+  directives: [ROUTER_DIRECTIVES]
 })
 export class AppComponent {
   title = 'turkopticon mockup';
-  showForm:boolean = false;
   footPaths = {
     paths: ['index', 'about', 'rules', 'faq'],
     map: {index: 'home', faq: 'FAQ'}
@@ -23,21 +21,6 @@ export class AppComponent {
     map: {},
     icon: {reviews: 'grid'}
   }
-  toggleForm(e?:any) {
-    if (e) e.preventDefault();
-    this.showForm = !this.showForm;
-  }
-  // constructor(private router:Router) { }
   constructor() {  }
 
 }
-
-
-// @Routes([
-//   {path: '/faq', component: FaqComponent},
-//   {path: '/about', component: AboutComponent},
-//   {path: '/index', component: IndexComponent},
-//   {path: '', component: IndexComponent},
-//   {path: '/reviews/:rid', component: ReviewsComponent},
-//   {path: '/reviews', component: ReviewsComponent}
-// ])
--- src/app/review-group/review-overview.component.ts
+++ src/app/review-group/review-overview.component.ts
@@ -1,17 +1,25 @@
 import { Component, OnChanges, Input } from '@angular/core';
+import { ReviewFormComponent } from '../review-form';
 
 @Component({
   moduleId: module.id,
   selector: 'review-overview',
   templateUrl: 'review-overview.component.html',
-  styleUrls: ['review-overview.component.css']
+  styleUrls: ['review-overview.component.css'],
+  directives: [ReviewFormComponent]
 })
 export class ReviewOverviewComponent implements OnChanges {
 
   @Input() overviewData:any;
   searchLink:string;
+  showForm:boolean = false;
   private API = 'https://www.mturk.com/mturk/searchbar';
   constructor() {}
+
+  toggleForm(e?:any) {
+    if (e) e.preventDefault();
+    this.showForm = !this.showForm;
+  }
 
   ngOnChanges() {
     console.log('ro',this.overviewData);
