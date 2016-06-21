1980f333e9b678d34a903da50e90b2f6aac48d59
Yuankai Li <ykzihel@gmail.com>
2016-06-21 11:44:58 -0400

    refactor(reviews): remove superfluous logging

--- src/app/routes/+reviews/reviews.component.ts
+++ src/app/routes/+reviews/reviews.component.ts
@@ -27,18 +27,8 @@ export class ReviewsComponent implements OnInit {
     private _ar:ActivatedRoute) {
   }
 
-  // routerOnActivate(curr:RouteSegment, tree?:RouteTree){
-  //   this.param = curr.getParam('rid');
-  //   this.fetchReviews(8);
-  // }
-
   fetchReviews(n:number, id:string) {
-    console.log('id: ',id);
-    this.reviewService.getReviews(n, {rid: id }).then(v => { console.log('v: ', v); return v; })
-      // .then(v => { this.source = v; console.log('initial creation:',this.source); })
-      // .then(v => Observable.from(v, m => 'reviews' in m ? m.reviews : m))
-      // .then(v => v.zip(Observable.interval(2000), p => p))
-      // .then(v => v.subscribe(console.log.bind(console)))
+    this.reviewService.getReviews(n, {rid: id })
       .then(v => (this.reviewData = Promise.resolve(v)) && v )
       .then(v => this.overviewData = Promise.resolve(v.length === 1 && 'reviews' in v[0] ? getOverview(v[0]) : null));
     function getOverview(obj) {
@@ -48,12 +38,11 @@ export class ReviewsComponent implements OnInit {
     }
   }
   ngOnInit() {
-    console.log(this._ar);
-    this.routeSub = this._ar.params.subscribe(p => {
-      console.log(p);
-      this.fetchReviews(8, p['rid']);
-    });
-    // console.log(this.location);
+    // console.log(this._ar);
+    // console.log(this._router);
+    this.routeSub = this._ar.params
+      .map(p => p['rid'])
+      .subscribe(rid => this.fetchReviews(8, rid));
   }
   ngOnDestroy() {
     this.routeSub.unsubscribe();
